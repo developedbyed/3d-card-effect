17eab7c94796c0e6d911b749171673fb492cbed7
Anmol Agrawal <anmol.ag53@gmail.com>
2020-10-30 01:48:04 +0530

    Improved animation's x-axis

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const sizes = document.querySelector(".sizes");
 
 //Moving Animation Event
 container.addEventListener("mousemove", (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let xAxis = (window.innerWidth / 2 - e.pageX) / -25;
   let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
