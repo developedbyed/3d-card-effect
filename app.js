d1b0817d03030e8907007c6eaa33fbd05cbc4592
Michael Chernomazov <michael23black@yandex.ru>
2020-10-31 14:39:50 +0300

    Fixed animation for different positions on the page

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ const description = document.querySelector(".info h3");
 const sizes = document.querySelector(".sizes");
 
 //Moving Animation Event
-container.addEventListener("mousemove", (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+container.addEventListener("mousemove", ({ pageX, pageY }) => {
+  const { top: topIndent, left: leftIndent, width, height } = container.getBoundingClientRect();
+  let xAxis = -((pageX - leftIndent) - width / 2) / 20;
+  let yAxis = ((pageY - topIndent) - height / 2) / 20;
   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
 //Animate In
